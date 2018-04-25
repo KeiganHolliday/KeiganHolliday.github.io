9e87e259743e482e4b32060af98baa2710755b24
Keigan Holliday <keiganholliday@mybrcc.edu>
2018-04-25 14:19:25 -0500

    Upload W/ Skeleton

---
+++ projects/fizzbuzz/fizzbuzz.js
@@ -0,0 +1,9 @@
+
+function fizzbuzz() {
+        for (var n = 1; n <= 100; n++){
+            document.write(i + "<br>")
+        }
+
+        if (n % 3 == 0 && n % 5 == 0){
+            document.write('pop')
+        }else if 
